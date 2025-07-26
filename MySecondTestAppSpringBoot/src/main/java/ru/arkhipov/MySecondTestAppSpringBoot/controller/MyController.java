7b69b60baf35389ff71828e8b3a12efc78ae4c39
danila <danil.kab.56@gmail.com>
2025-07-26 19:11:15 +0500

    lr2

---
+++ MySecondTestAppSpringBoot/src/main/java/ru/arkhipov/MySecondTestAppSpringBoot/controller/MyController.java
@@ -0,0 +1,75 @@
+package ru.arkhipov.MySecondTestAppSpringBoot.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.arkhipov.MySecondTestAppSpringBoot.model.Request;
+import ru.arkhipov.MySecondTestAppSpringBoot.model.Response;
+import ru.arkhipov.MySecondTestAppSpringBoot.service.ValidationService;
+import ru.arkhipov.MySecondTestAppSpringBoot.exception.ValidationFailedException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class MyController {
+
+    private final ValidationService validationService;
+
+    @Autowired
+    public MyController(ValidationService validationService) {
+        this.validationService = validationService;
+    }
+
+    @PostMapping(value = "/feedback")
+    public ResponseEntity<Response> feedback(@Valid @RequestBody Request request,
+                                             BindingResult bindingResult) {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+
+        Response response = Response.builder()
+                .uid(request.getUid())
+                .operationUid(request.getOperationUid())
+                .systemTime(simpleDateFormat.format(new Date()))
+                .code("success")
+                .errorCode("")
+                .errorMessage("")
+                .build();
+
+        try {
+            validationService.isValid(bindingResult);
+
+            if ("123".equals(request.getUid())) {
+                throw new ru.arkhipov.MySecondTestAppSpringBoot.exception.UnsupportedCodeException(
+                        "UID 123 запрещен"
+                );
+            }
+
+        } catch (ValidationFailedException e) {
+            response.setCode("failed");
+            response.setErrorCode("ValidationException");
+            response.setErrorMessage("Ошибка валидации");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
+        } catch (ru.arkhipov.MySecondTestAppSpringBoot.exception.UnsupportedCodeException e) {
+            response.setCode("failed");
+            response.setErrorCode("UnsupportedCodeException");
+            response.setErrorMessage("UID 123 запрещен");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
+        } catch (Exception e) {
+            response.setCode("failed");
+            response.setErrorCode("UnknownException");
+            response.setErrorMessage("Произошла непредвиденная ошибка");
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
